84fef1f37b06944e24292d91aa8eb98a1412af45
Gradia Mutambay <gradimutdev@gmail.com>
2019-08-12 01:07:12 +0300

    utils folder created

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
+// jshint esversion : 6
+import VueSocketIo from 'vue-socket.io';
+import VueResouce from 'vue-resource';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { url } from './utils/config';
+
 
 Vue.config.productionTip = false;
+
+// Socket configuration
+Vue.use(new VueSocketIo({
+  debug: true,
+  connection: `${url}/video-chat`,
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_',
+  },
+}));
+
+// Vue resource for Http
+Vue.use(VueResouce);
 
 new Vue({
   router,
--- src/store.js
+++ src/store.js
@@ -1,16 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { STATUS_OPTIONS } from './utils/config';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    room: undefined, // current room
+    username: undefined, // user
+    status: STATUS_OPTIONS.available, // user status
+    rooms: [], // Available rooms in the whole chat
   },
   mutations: {
-
+    // Mutation per action (joinRoom, changeRoom, setRooms, leaveChat, changeSatus)
   },
   actions: {
-
+    // Actioin to be trigger when :
+    // joinRoom, changeRoom, setRooms, leaveChat, changeStatus.
   },
 });
